e4e892bd74f97313e7ad238c59b207c7b7b72849
ppetrid <ppetrid@yawd.eu>
2013-05-27 13:07:46 +0300

    django.conf.urls.defaults is deprecated

--- paypal/standard/ipn/urls.py
+++ paypal/standard/ipn/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('paypal.standard.ipn.views',            
     url(r'^$', 'ipn', name="paypal-ipn"),
